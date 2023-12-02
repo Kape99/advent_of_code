dcf561454fded88eacd939f431d6005423281ae4
Kape99 <lucapecchi13@gmail.com>
2023-12-02 21:29:25 +0100

    day2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,10 +2,56 @@ use std::{collections::HashMap, fs::read_to_string};
 
 fn main() {
     let games = read_lines("input.txt");
-    let mut game_sum = 0;
+    
+    let mut count = 0;
     for game in &games {
         let (game_id, round_str) = get_id_and_rounds(game);
         
+        let round_list_str: Vec<_> = round_str[1..].split(";").collect();
+    
+        let mut bag = HashMap::from([(" red", 0), (" green", 0), (" blue", 0)]);
+
+        for round in round_list_str {
+
+            let cubes_str: Vec<_> = round[1..].split(", ").collect();
+        
+            for cube_str in cubes_str {
+                let (color, number) = match cube_str.find(" ") {
+                    Some(split_index) => {
+                        let (n, c) = cube_str.split_at(split_index);
+                        let num = match n.parse::<i32>() {
+                            Ok(id) => id,
+                            Err(msg) => panic!("Error: {msg}"),
+                        };
+        
+                        (c, num)
+                    }
+                    None => panic!("the cube divisor was not found"),
+                };
+                if bag.get(color).unwrap() < &number{
+                    *bag.get_mut(color).unwrap() = number;
+                }
+            }
+        }
+        let mut exp = 1;
+        for val in bag.values(){
+            exp *= val;
+
+        }
+        count += exp;
+        
+    }
+
+    println!("{count}");
+    // let game_sum = compute_valid_games(games);
+    // println!("Game sum: {game_sum}");
+}
+
+fn compute_valid_games(games: Vec<String>) -> u32{
+    let mut game_sum = 0;
+    for game in &games {
+        let (game_id, round_str) = get_id_and_rounds(game);
+    
         let is_valid = is_valid_game(&round_str);
 
         match is_valid {
@@ -14,7 +60,7 @@ fn main() {
         }
 
     }
-    println!("Game sum: {game_sum}");
+    game_sum
 }
 
 fn is_valid_game(round_str: &str) -> bool {
