51dc8a24494daa908014629ae6eb4d6e3ec0e101
Kape99 <lucapecchi13@gmail.com>
2023-12-02 21:30:21 +0100

    rust fmt

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,19 +2,18 @@ use std::{collections::HashMap, fs::read_to_string};
 
 fn main() {
     let games = read_lines("input.txt");
-    
+
     let mut count = 0;
     for game in &games {
         let (game_id, round_str) = get_id_and_rounds(game);
-        
+
         let round_list_str: Vec<_> = round_str[1..].split(";").collect();
-    
+
         let mut bag = HashMap::from([(" red", 0), (" green", 0), (" blue", 0)]);
 
         for round in round_list_str {
-
             let cubes_str: Vec<_> = round[1..].split(", ").collect();
-        
+
             for cube_str in cubes_str {
                 let (color, number) = match cube_str.find(" ") {
                     Some(split_index) => {
@@ -23,23 +22,21 @@ fn main() {
                             Ok(id) => id,
                             Err(msg) => panic!("Error: {msg}"),
                         };
-        
+
                         (c, num)
                     }
                     None => panic!("the cube divisor was not found"),
                 };
-                if bag.get(color).unwrap() < &number{
+                if bag.get(color).unwrap() < &number {
                     *bag.get_mut(color).unwrap() = number;
                 }
             }
         }
         let mut exp = 1;
-        for val in bag.values(){
+        for val in bag.values() {
             exp *= val;
-
         }
         count += exp;
-        
     }
 
     println!("{count}");
@@ -47,27 +44,26 @@ fn main() {
     // println!("Game sum: {game_sum}");
 }
 
-fn compute_valid_games(games: Vec<String>) -> u32{
+fn compute_valid_games(games: Vec<String>) -> u32 {
     let mut game_sum = 0;
     for game in &games {
         let (game_id, round_str) = get_id_and_rounds(game);
-    
+
         let is_valid = is_valid_game(&round_str);
 
         match is_valid {
             true => game_sum += game_id,
             _ => {}
         }
-
     }
     game_sum
 }
 
 fn is_valid_game(round_str: &str) -> bool {
     let round_list_str: Vec<_> = round_str[1..].split(";").collect();
-    
+
     for round in round_list_str {
-        if !is_valid_round(round){
+        if !is_valid_round(round) {
             return false;
         }
     }
@@ -75,7 +71,7 @@ fn is_valid_game(round_str: &str) -> bool {
     true
 }
 
-fn is_valid_round(round: &str) -> bool{
+fn is_valid_round(round: &str) -> bool {
     let bag = HashMap::from([(" red", 12), (" green", 13), (" blue", 14)]);
 
     let cubes_str: Vec<_> = round[1..].split(", ").collect();
@@ -116,14 +112,9 @@ fn get_id_and_rounds(game: &str) -> (u32, String) {
         Ok(id) => id,
         Err(msg) => panic!("Error: {msg}"),
     };
-    
+
     (game_id, rounds_str.to_owned())
 }
-
-
-
-
-
 
 fn read_lines(filename: &str) -> Vec<String> {
     let mut result = Vec::new();
